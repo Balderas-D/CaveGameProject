b0a912ec1c5a78779754b10a72f63444b07fab0d
Balderas-D <142932408+Balderas-D@users.noreply.github.com>
2025-04-28 15:14:47 -0700

    added level loader features

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -8,10 +8,17 @@ public class LevelLoader : MonoBehaviour
     public static int SceneNumber;
     public GameObject player;
     AudioSource m_AudioSource;
+    public int enemyNumber = 3;
 
     public void Start()
     {
         m_AudioSource = GameObject.FindObjectOfType<AudioSource>();
+        enemyNumber = GameObject.FindObjectsOfType<Enemy>().Length;
+    }
+
+    public void Update()
+    {
+        enemyNumber = GameObject.FindObjectsOfType<Enemy>().Length;
     }
 
     private void Awake()
@@ -27,55 +34,70 @@ public class LevelLoader : MonoBehaviour
 
     public void NextLevel()
     {
-        Debug.Log(SceneNumber);
-        SceneNumber++;
-        Debug.Log(SceneNumber);
-        if (SceneNumber < SceneManager.sceneCountInBuildSettings)
+        if (enemyNumber == 0)
         {
-            PlayerPrefs.SetFloat("Y", player.transform.position.y);
-            PlayerPrefs.SetString("Start", "Left");
-            SceneManager.LoadScene(SceneNumber);
+            Debug.Log(SceneNumber);
+            SceneNumber++;
+            Debug.Log(SceneNumber);
+            if (SceneNumber < SceneManager.sceneCountInBuildSettings)
+            {
+                PlayerPrefs.SetFloat("Y", player.transform.position.y);
+                PlayerPrefs.SetString("Start", "Left");
+                SceneManager.LoadScene(SceneNumber);
+            }
+            PlayerPrefs.SetFloat("ClipTime", m_AudioSource.time);
         }
-        PlayerPrefs.SetFloat("ClipTime", m_AudioSource.time);
     }
 
     public void Up()
     {
-        Debug.Log(SceneNumber);
-        SceneNumber++;
-        Debug.Log(SceneNumber);
-        if (SceneNumber < SceneManager.sceneCountInBuildSettings)
+        if (enemyNumber == 0)
         {
-            PlayerPrefs.SetString("Start", "Bottom");
-            SceneManager.LoadScene(SceneNumber);
+
+
+            Debug.Log(SceneNumber);
+            SceneNumber++;
+            Debug.Log(SceneNumber);
+            if (SceneNumber < SceneManager.sceneCountInBuildSettings)
+            {
+                PlayerPrefs.SetString("Start", "Bottom");
+                SceneManager.LoadScene(SceneNumber);
+            }
+            PlayerPrefs.SetFloat("ClipTime", m_AudioSource.time);
         }
-        PlayerPrefs.SetFloat("ClipTime", m_AudioSource.time);
     }
 
     public void Down()
     {
-        SceneNumber--;
-
-        if (SceneNumber >= 0)
+        if (enemyNumber == 0)
         {
-            PlayerPrefs.SetString("Start", "Top");
-            SceneManager.LoadScene(SceneNumber);
+            SceneNumber--;
+
+            if (SceneNumber >= 0)
+            {
+                PlayerPrefs.SetString("Start", "Top");
+                SceneManager.LoadScene(SceneNumber);
+            }
+            PlayerPrefs.SetFloat("ClipTime", m_AudioSource.time);
         }
-        PlayerPrefs.SetFloat("ClipTime", m_AudioSource.time);
     }
 
 
         public void LastLevel()
     {
-        SceneNumber--;
-
-        if (SceneNumber >= 0)
+        if (enemyNumber == 0)
         {
-            PlayerPrefs.SetFloat("Y", player.transform.position.y);
-            PlayerPrefs.SetString("Start", "Right");
-            SceneManager.LoadScene(SceneNumber);
+
+            SceneNumber--;
+
+            if (SceneNumber >= 0)
+            {
+                PlayerPrefs.SetFloat("Y", player.transform.position.y);
+                PlayerPrefs.SetString("Start", "Right");
+                SceneManager.LoadScene(SceneNumber);
+            }
+            PlayerPrefs.SetFloat("ClipTime", m_AudioSource.time);
         }
-        PlayerPrefs.SetFloat("ClipTime", m_AudioSource.time);
     }
 
     public void PlayGame()
